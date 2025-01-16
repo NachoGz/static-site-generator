81d7efaf1f0bb77827f096a78724336be17b7608
NachoGz <ignaciogomezf1@gmail.com>
2025-01-16 12:25:13 -0300

    fix bug when checking if value is None

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,14 +3,14 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag = None, value = None, props = None):
-        if not value:
+        if value is None:
             raise ValueError("value is required")
 
         super().__init__(tag, value, None, props)
     
     # renders a leaf node as an HTML string
     def to_html(self) -> str:
-        if not self.value:
+        if self.value is None:
             raise ValueError
         elif not self.tag:
             return self.value
